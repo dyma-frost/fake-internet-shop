a61dc68dd679155134aff587a972d8a8e3c9aa6a
dyma.frost <dyma.frost@gmail.com>
2023-02-09 18:06:43 +0200

    create Content.css and import him

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Content } from './Content'
 import Title from './Title'
 
-
-
-
 //component name OlwaysInPaskalCase
 
 function App() {
@@ -14,8 +11,8 @@ function App() {
             <Title text="React" />
             <Title text="TS" />
             <Content
-                text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt minima, eligendi, eius omnis mollitia libero alias necessitatibus nihil molestiae eveniet eos sint et exercitationem harum quod! Dolores aliquid alias voluptatibus!"
-                text2="Hello world 2"
+                text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt minima, eligendi, eius omnis mollitia libero "
+                text2="alias necessitatibus nihil molestiae eveniet eos sint et exercitationem harum quod! Dolores aliquid alias voluptatibus!"
                 year={2023}
             />
         </>
