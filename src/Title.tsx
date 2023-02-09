578d4646c5527000f89743d1b166d6a08dcf2b35
dyma.frost <dyma.frost@gmail.com>
2023-02-09 18:34:38 +0200

    remove files and add absolute imports

--- src/Title.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-    text: string
-}
-
-const Title = (props: Props) => {
-    return <h1>Hello {props.text}</h1>
-}
-export default Title
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './Content'
-import Title from './Title'
-
-//component name OlwaysInPaskalCase
-
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="TS" />
-            <Content
-                text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt minima, eligendi, eius omnis mollitia libero "
-                text2="alias necessitatibus nihil molestiae eveniet eos sint et exercitationem harum quod! Dolores aliquid alias voluptatibus!"
-                year={2023}
-            />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
