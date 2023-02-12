f9b13ac9d10dafad4832e0759c3944780de00d02
dyma.frost <dyma.frost@gmail.com>
2023-02-12 20:40:39 +0200

    add productsArray

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,7 +1,16 @@
 import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
+import productsArray from 'utils/productsArray'
 
 type Props = {}
+
+type ProductProps = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 
 const ProductsList = (props: Props) => {
     return (
@@ -11,36 +20,25 @@ const ProductsList = (props: Props) => {
             </Typography>
 
             <Grid container spacing={3}>
-                <Grid item xs={12} sm={6} md={4}>
-                    {' '}
-                    <ProductsListItem
-                        title="iPhone 14 Pro"
-                        description="This is iPhone 14 Pro"
-                        type="Phone"
-                        capacity='512'
-                        price={2000}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    {' '}
-                    <ProductsListItem
-                        title="iPhone 12 Pro"
-                        description="This is iPhone 12 Pro"
-                        type="Phone"
-                        capacity='128'
-                        price={1200}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    {' '}
-                    <ProductsListItem
-                        title="iPhone 13 Pro"
-                        description="This is iPhone 13 Pro"
-                        type="Phone"
-                        capacity='256'
-                        price={1800}
-                    />
-                </Grid>
+                {productsArray.map(
+                    ({
+                        title,
+                        description,
+                        type,
+                        capacity,
+                        price,
+                    }: ProductProps) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ProductsListItem
+                                title={title}
+                                description={description}
+                                type={type}
+                                capacity={capacity}
+                                price={price}
+                            />
+                        </Grid>
+                    )
+                )}
             </Grid>
         </>
     )
