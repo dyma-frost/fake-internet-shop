50ec0a587b8d73283221e75de49e20b353a964dd
dyma.frost <dyma.frost@gmail.com>
2023-02-12 19:57:44 +0200

    add props to ProductsListItem

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -13,15 +13,33 @@ const ProductsList = (props: Props) => {
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 14 Pro"
+                        description="This is iPhone 14 Pro"
+                        type="Phone"
+                        capacity='512'
+                        price={2000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 12 Pro"
+                        description="This is iPhone 12 Pro"
+                        type="Phone"
+                        capacity='128'
+                        price={1200}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 13 Pro"
+                        description="This is iPhone 13 Pro"
+                        type="Phone"
+                        capacity='256'
+                        price={1800}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -1,17 +1,25 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import './ProductsListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 
 const ProductsListItem = (props: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product-title">iPhone 14 Pro</div>
-                <div className="product-desc">This iPhone 14 Pro</div>
-                <div className="product-features">Type: Phone</div>
-                <div className="product-features">Capacity: 256 Gb</div>
-                <div className="product-price">1200$</div>
+                <div className="product-title">{props.title}</div>
+                <div className="product-desc">{props.description}</div>
+                <div className="product-features">Type: {props.type}</div>
+                <div className="product-features">
+                    Capacity: {props.capacity} GB
+                </div>
+                <div className="product-price">{props.price}$</div>
             </CardContent>
             <CardActions className="product-btn-wrap">
                 <Button variant="outlined">Add to cart</Button>
