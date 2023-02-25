5677f30b832300336d0bc62fca28b3dd17d16cd7
dyma.frost <dyma.frost@gmail.com>
2023-02-25 22:12:02 +0200

    add React Router and coonect to app

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,12 @@
-import Header from 'container/Header/Header'
-import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from 'pages/Home/Home'
+import { Container } from '@mui/system'
+import CartPage from 'pages/Cart/CartPage'
 
 type Props = {}
 
@@ -28,7 +31,19 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <Main addProductToCart={addProductToCart} />
+            <Container
+                sx={{
+                    padding: '60px 0',
+                }}
+            >
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="cart" element={<CartPage />} />
+                </Routes>
+            </Container>
             <Footer />
         </StyledEngineProvider>
     )
