06a634af2a43bc9233d14b86e14ce3e17445b230
dyma.frost <dyma.frost@gmail.com>
2023-02-28 16:07:19 +0200

    add CartTotal and CartProductList

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -41,7 +41,7 @@ const App = (props: Props) => {
                         path="/"
                         element={<Home addProductToCart={addProductToCart} />}
                     />
-                    <Route path="cart" element={<CartPage />} />
+                    <Route path="cart" element={<CartPage productsInCart={productsInCart}/>} />
                 </Routes>
             </Container>
             <Footer />
